706836776c85c5a1cdd0b39e12b757d4ba8301b3
Amrita Tripathy <116503330+amthy1996@users.noreply.github.com>
2022-10-25 12:51:29 +0530

    Update streamlit_app.py
    added 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,16 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("Select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit list is:")
+streamlit.dataframe(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit list is:")
+streamlit.dataframe(my_data_rows)
 
 
